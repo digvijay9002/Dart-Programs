faf72a6669510d3e9d8767c09a69504d226eeac6
Digvijay Chavda <54387471+digvijay9002@users.noreply.github.com>
2022-08-10 10:42:38 +0530

    Create demo3.dart

---
+++ If else/demo3.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  var ch='E';
+
+  if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' || 
+       ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+  {
+    print("${ch} is Vowel.");
+  }
+  else
+  {
+    print("${ch} is Consonant.");
+  }
+}
